e58c6e8bcedaf56e599f0c10d29881e1ac7b863b
nyan lin tun <devkonyan@gmail.com>
2019-06-16 16:43:20 +0630

    Updates

---
+++ precache-manifest.f8c4ea983016471f3d27e8615623d979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ba7e7d7ce8c50e34a159196ecad2ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1c82d03afc14338492",
+    "url": "/static/css/main.d0420d63.chunk.css"
+  },
+  {
+    "revision": "e31e657259a4219a8ee3",
+    "url": "/static/js/2.412be5fd.chunk.js"
+  },
+  {
+    "revision": "5d1c82d03afc14338492",
+    "url": "/static/js/main.6d28fbce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
